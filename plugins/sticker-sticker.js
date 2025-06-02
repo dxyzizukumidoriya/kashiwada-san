4f01aa6aa043eafe6a4bedee6d2c4f7eb9e7747e
DekuGz - Leooxzy <151708571+dxyzizukumidoriya@users.noreply.github.com>
2025-06-02 08:29:58 +0700

    Create sticker-sticker.js

---
+++ plugins/sticker-sticker.js
@@ -0,0 +1,35 @@
+// 🔥® Rin-Okumura™ 🔥
+// 👿 Creator: Dxyz
+// ⚡ Plugin: sticker-sticker.js
+
+module.exports = {
+    help: ['s', 'sticker', 'stk', 'stiker'],
+    tags: ['sticker'],
+    command: ['s', 'sticker', 'stk', 'stiker'],
+    limit: true,
+    code: async (m, {
+        conn,
+        usedPrefix,
+        command
+    }) => {
+        const q = m.quoted ? m.quoted : m;
+        if (!q) return m.reply(` *[ ! ]* Send/Reply Images/Videos/Gifs With Captions ${usedPrefix + command}\nVideo Duration 1-9 Seconds`)
+        const mime = (q.msg || q).mimetype || ''
+        if (/image/.test(mime)) {
+            const media = await q.download()
+            await conn.sendImageAsSticker(m.chat, media, m, {
+                packname: config.name,
+                author: `By: ${config.ownername} / https://wa.me/${config.owner}`
+            })
+        } else if (/video/.test(mime)) {
+            if ((q.msg || q).seconds > 11) return
+            const media = await q.download()
+            await conn.sendVideoAsSticker(m.chat, media, m, {
+                packname: config.name,
+                author: `By: ${config.ownername} / https://wa.me/${config.owner}`
+            })
+        } else {
+            m.reply(` *[ ! ]* Send/Reply Images/Videos/Gifs With Captions ${usedPrefix + command}\nVideo Duration 1-9 Seconds`)
+        }
+    }
+}
