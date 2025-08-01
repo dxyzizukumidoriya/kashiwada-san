6bd57afaec1d69f14e327e0404c55435baa3d27b
DekuGz - Leooxzy <151708571+dxyzizukumidoriya@users.noreply.github.com>
2025-08-01 19:29:41 +0700

    Update sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -19,14 +19,14 @@ module.exports = {
             const media = await q.download()
             await conn.sendImageAsSticker(m.chat, media, m, {
                 packname: config.name,
-                author: `By: ${config.ownername} / https://wa.me/${config.owner}`
+                author: `By: ${config.ownername}`
             })
         } else if (/video/.test(mime)) {
             if ((q.msg || q).seconds > 11) return
             const media = await q.download()
             await conn.sendVideoAsSticker(m.chat, media, m, {
                 packname: config.name,
-                author: `By: ${config.ownername} / https://wa.me/${config.owner}`
+                author: `By: ${config.ownername}`
             })
         } else {
             m.reply(` *[ ! ]* Send/Reply Images/Videos/Gifs With Captions ${usedPrefix + command}\nVideo Duration 1-9 Seconds`)
